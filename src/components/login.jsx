c7c88edfcb4c3d4c90400f29ef07f4f8d88efcbf
Sarin <sarinmsari@gmail.com>
2020-07-29 14:11:54 +0530

    login form validation

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,24 +1,98 @@
 import React, { Component } from "react";
-import './login.css';
+import "./login.css";
+import { Link } from "react-router-dom";
+
+const validEmailRegex = RegExp(
+  /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+);
+const validateForm = (error) => {
+  let valid = true;
+  Object.values(error).forEach((val) => val.length > 0 && (valid = false));
+  return valid;
+};
 
 class login extends Component {
-  state = {};
+  state = {
+    email: null,
+    password: null,
+    error: {
+      email: "",
+      password: "",
+    },
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    event.preventDefault();
+    if (validateForm(this.state.error)) {
+      console.info("Valid Form");
+    } else {
+      console.error("Invalid Form");
+    }
+  };
+
+  handleChange = (event) => {
+    event.preventDefault();
+    const { name, value } = event.target;
+    let error = this.state.error;
+
+    switch (name) {
+      case "email":
+        error.email = validEmailRegex.test(value) ? "" : "Email is not valid!";
+        break;
+      case "password":
+        error.password =
+          value.length < 8
+            ? "Password must be at least 8 characters long!"
+            : "";
+        break;
+      default:
+        break;
+    }
+
+    this.setState({ error, [name]: value });
+  };
+
   render() {
     return (
       <div className="container">
         <div className="form-wrapper">
-            <div className="header">LOG IN</div>
-            <form className="login-form">
-                <div className="email">
-                    <label htmlFor="email">Email:</label>
-                    <input type="email" name="email" id="email" placeholder="email"/>
-                </div>
-                <div className="password">
-                    <label htmlFor="password">Password:</label>
-                    <input type="password" name="password" id="password" placeholder="password"/>
-                </div>
-                <button type="submit">Log In</button>
-            </form>
+          <div className="header">LOG IN</div>
+          <form className="login-form" onSubmit={this.handleSubmit} noValidate>
+            <div className="email">
+              <label htmlFor="email">Email:</label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="email"
+                onChange={this.handleChange}
+                noValidate
+              />
+              <br />
+              {this.state.error.email.length > 0 && (
+                <span className="error">{this.state.error.email}</span>
+              )}
+            </div>
+            <div className="password">
+              <label htmlFor="password">Password:</label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="password"
+                onChange={this.handleChange}
+                noValidate
+              />
+              <br />
+              {this.state.error.password.length > 0 && (
+                <span className="error">{this.state.error.password}</span>
+              )}
+            </div>
+            
+              <button type="submit">Log In</button>
+            
+          </form>
         </div>
       </div>
     );
